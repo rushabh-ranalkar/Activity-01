a1873a811cc71ef198386d6f36ff857dbd45947e
rushabh-ranalkar <58594200+rushabh-ranalkar@users.noreply.github.com>
2019-12-06 20:30:21 +0530

    Update swipe.c

--- swipe.c
+++ swipe.c
@@ -7,10 +7,10 @@ int main()
   b=20;
   swap(&a,&b);
   printf("a=%d,b=%d\n",a,b);
-  return 0
+  return 0;
 }
-void swap(int p1,int *p2) {
-   t=*p1;
+void swap(int *p1,int *p2) {
+  int t=*p1;
   *p1=*p2;
   *p2=t;
 }
